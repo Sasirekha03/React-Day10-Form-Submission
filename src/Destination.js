d0b4ea3c1455856a8e0104bbde466d92bb0604f7
Sasirekha03 <108916484+Sasirekha03@users.noreply.github.com>
2023-04-06 06:43:11 +0530

    Update Destination.js

--- src/Destination.js
+++ src/Destination.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Destination() {
   return (
-    <div>This is Destination Page</div>
+    <div>
+    <h2>This is Destination Page</h2>
+    <ul>
+            <li>1. Ladak</li>
+            <li>2. Switzerland</li>
+            <li>3. Greenland</li>
+            <li>4. Iceland</li>
+            <li>5. Las Vegas</li>
+        </ul>
+    </div>
   )
 }
 
