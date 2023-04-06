07e311622029758adfe6c6b0106f076933b5ee59
Sasirekha03 <108916484+Sasirekha03@users.noreply.github.com>
2023-04-06 06:28:12 +0530

    Update SubmittedForm.js

--- src/SubmittedForm.js
+++ src/SubmittedForm.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React from 'react';
 
-function SubmittedForm({formValues}) {
+function SubmittedForm({ Name,email,errorMsg,phoneNumber,pass, travelDate,returnDate, dest, res,isValid,Valid }) {
   return (
     <div>
-        <h3>
-            name:{formValues.map(t=><h1>{t}</h1>)};
-        </h3>
+      <h2>Entered Values</h2>
+      <form>
+        <label>
+          Name: {Name}
+          </label>
+          <br/>
+          <br/>
+          <label>
+        {Valid ? <p> email: {email}</p> : <p>email: Email is invalid</p>}
+        </label>
+        <br/>
+        <label>
+        {isValid ?<p> Phone :{phoneNumber}</p> : <p> Phone:Phone number is invalid</p>}
+        </label>
+        <br/>
+        <label>
+          Passport:{pass}
+          </label>
+          <br/>
+          <br/>
+        <label>
+          TravelDate:{travelDate}
+          </label>
+          <br/>
+          <br/>
+          Return Date:
+        <label>
+        {errorMsg ? <p style={{ color: 'red' }}>{errorMsg}</p>:<p>{returnDate}</p>}
+          </label>
+          <br/>
+          <br/>
+        <label>
+          Destination:{dest}
+        </label>
+        <br />
+        <br/>
+        <label>
+          Reason for Travel:{res}
+        </label>
+        <br />
+        <br/>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SubmittedForm
+export default SubmittedForm;
