d2964d23e26ffbb866a46ec93c6d8c7f894e3cb1
Sasirekha03 <108916484+Sasirekha03@users.noreply.github.com>
2023-04-06 06:49:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from 'react';
 import {  BrowserRouter,Routes, Route } from 'react-router-dom';
-//import BookList from './Booklist'
+
 import Destination from './Destination'
 import Home from './Home';
 import About from './About';
  import Contact from './Contact';
 import Naving from './Naving';
 import RegistrationForm from './RegistrationForm';
-//import SubmittedForm from './SubmittedForm';
-//import './App.css';
 
 const App = () => {
   return (
+   <div className="App">
     <BrowserRouter>
-    <div className="App">
-<Naving/>
-{/*<SubmittedForm/>*/}
 <Routes>
-<Route path="/"  element={<Home/>} />
+ 
+           <Route path="/" element={<Naving />}>
+          <Route index element={<Home />} />
           <Route path="/about" element={<About/>} />
           <Route path="/destination" element={<Destination/>} />
           <Route path="/contact" element={<Contact/>} />
           <Route path="/registrationform" element={<RegistrationForm/>} />
-          {/*<Route path="/submittedform" element={<SubmittedForm/>} />*/}
           
 </Routes>
-    </div>
-  </BrowserRouter>
-
-
+</BrowserRouter>
+</div>
    
   );
 };
